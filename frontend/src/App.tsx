532f7e77b568278732fd6214dc03180d7e566b6f
weiqun <1348172474@qq.com>
2026-03-23 16:50:09 +0800

    feat: add preview routes to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,25 +5,36 @@ import Dashboard from './pages/Dashboard'
 import Rules from './pages/Rules'
 import Jobs from './pages/Jobs'
 import Articles from './pages/Articles'
+import Preview from './pages/Preview'
+import ArticleDetail from './pages/Preview/ArticleDetail'
 
 const { Content } = Layout
 
 function App() {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sidebar />
-      <Layout>
-        <Content style={{ margin: '16px', padding: '24px', background: '#fff' }}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/rules" element={<Rules />} />
-            <Route path="/jobs" element={<Jobs />} />
-            <Route path="/articles" element={<Articles />} />
-          </Routes>
-        </Content>
-      </Layout>
-    </Layout>
+    <Routes>
+      {/* 管理后台 */}
+      <Route path="/" element={
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sidebar />
+          <Layout>
+            <Content style={{ margin: '16px', padding: '24px', background: '#fff' }}>
+              <Routes>
+                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/rules" element={<Rules />} />
+                <Route path="/jobs" element={<Jobs />} />
+                <Route path="/articles" element={<Articles />} />
+              </Routes>
+            </Content>
+          </Layout>
+        </Layout>
+      } />
+
+      {/* 预览服务 */}
+      <Route path="/preview" element={<Preview />} />
+      <Route path="/preview/article/:id" element={<ArticleDetail />} />
+    </Routes>
   )
 }
 
